7ad0ff3dcf807d406f86ce8626354abee503cd16
AlexandraShevchenko <lavandachereshenko@gmail.com>
2016-04-09 14:06:29 +0300

    Update pow.js

--- homework/javascript1/pow.js
+++ homework/javascript1/pow.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-    var numb = prompt("укажите число", '');
-    var exp = prompt('укажите степень', '');
+    var numb = prompt('enter a number', '');
+    var exp = prompt('enter an exponent', '');
     var result = Math.pow(numb, exp);
 
     console.log(result);
