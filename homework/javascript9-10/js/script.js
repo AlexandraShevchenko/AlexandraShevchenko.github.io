710ebe9f896cc6c82a5ec8c9f516b622f863b96b
AlexandraShevchenko <lavandachereshenko@gmail.com>
2016-05-17 12:29:33 +0300

    Update script.js

--- homework/javascript9-10/js/script.js
+++ homework/javascript9-10/js/script.js
@@ -1,36 +1,13 @@
 (function($) {
     $(function() {
-        $('.jcarousel').jcarousel();
+        $('[data-jcarousel]').each(function() {
+            var el = $(this);
+            el.jcarousel(el.data());
+        });
 
-        $('.jcarousel-control-prev')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('.jcarousel-control-next')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
-
-        $('.jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                $(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                $(this).removeClass('active');
-            })
-            .jcarouselPagination();
+        $('[data-jcarousel-control]').each(function() {
+            var el = $(this);
+            el.jcarouselControl(el.data());
+        });
     });
 })(jQuery);
