430435ea2996e1b3f32ad0653998e90ee4c1cb0c
AlexandraShevchenko <lavandachereshenko@gmail.com>
2016-06-15 22:34:07 +0300

    Update script.js

--- homework/javascript15-16/js/script.js
+++ homework/javascript15-16/js/script.js
@@ -1,19 +1,41 @@
-function submit_handler(form) {
- alert(form.search.value);
- return false;
-}
+$(function(){
+  $('#sendform').submit(function(e) {
+    e.preventDefault();
 
-function GoogleCallback (func, data) {
-    window[func](data);
-}
 
-$.getJSON("http://ajax.googleapis.com/ajax/services/search/local?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=PHP&callback=GoogleCallback&context=?",
-function(data){
-    var ul = document.createElement("ul");
-    $.each(data.results, function(i, val){
+var text = $('#formdata').val();
+console.log("text",text)
+
+  $.ajax({
+             url:'https://pixabay.com/api/?key=1888689-32b6c3d1fa4ab52b9ed34d259&q= '+ text +' &image_type=photo',
+             dataType : "jsonp",
+
+             success: function(data){
+               console.log("data",data);
+
+               var ul = document.createElement("ul");
+
+               $.each(data.hits, function(i, val){
+
             var li = document.createElement("li");
-            li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;
+
+            console.log('data.hits',data.hits);
+
+           var inner = '<img class="photo" src="' + val.webformatURL + '">' +
+           '<p><a href="'+ val.pageURL+'" title="'+val.pageURL+'" target="_blank">'+val.pageURL+'</a></p>' +
+           '<p class="users">Фотограф: '+ val.user + '</p>';
+
+          li.innerHTML = inner;
+
+
             ul.appendChild(li);
+    });
+
+    $('#results').html(ul);
+
+
+  },
+
         });
-    $('body').html(ul);
-});
+    });
+  });
